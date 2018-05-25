1ac9a23e1141897101cd58dd6744019ac5f89325
poppai <poppai@example.com>
2018-05-25 11:09:01 +0900

    need_api_setting

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+  "fmt"
+  "github.com/ChimeraCoder/anaconda"
+  //"net/http"
+  "os/exec"
+  "time"
+)
+func errCheck(er error){
+  if er != nil {
+    panic(er)
+  }
+}
+func check(st string)string{
+  newSt := fmt.Sprintf("#NowPlaying: %s",getTrackInfo())
+  if st != newSt{
+    post(newSt)
+    return newSt
+  }
+  return st
+}
+//supervisor
+func GetTwitterAi() *anaconda.TwitterApi {//この関数で自前のアカウントでツイッターにアクセスする
+  anaconda.SetConsumerKey("")
+  anaconda.SetConsumerSecret("")
+  api := anaconda.NewTwitterApi("", "")
+  return api
+}
+func getTrackInfo()string{
+  infoAscii, err := exec.Command("osascript", "getTrack.scpt").Output()
+  errCheck(err)
+  info:=string(infoAscii)
+  return info
+}
+func post(text string){
+  api :=GetTwitterApi()
+  _, err := api.PostTweet(text,nil)
+  errCheck(err)
+}
+func main(){
+  tweet := fmt.Sprintf("#NowPlaying: %s",getTrackInfo())
+  for{
+    tweet =check(tweet)
+    time.Sleep(1)
+  }
+}
